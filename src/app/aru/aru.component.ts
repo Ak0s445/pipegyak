8045b68cc3b4a66f2348fa0c0e28af5f5758332a
Ak0s445 <cipolakacsoakos5@gmail.com>
2025-11-06 12:04:59 +0100

    datum es nagybetu

--- src/app/aru/aru.component.ts
+++ src/app/aru/aru.component.ts
@@ -1,13 +1,20 @@
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-aru',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [
+    CurrencyPipe, 
+    UpperCasePipe,
+    DatePipe
+  ],
   templateUrl: './aru.component.html',
   styleUrl: './aru.component.css'
 })
 export class AruComponent {
   ar=35;
+  isAdmin = true;
+  dateNow = new Date();
+
 }
